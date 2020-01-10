f8392ee55bd0cbe2c823e4fd76c8f3351c29b728
Valentin Sergeevich Shapovalov <apovalov@gmail.com>
2020-01-11 01:11:37 +0300

    Gesture recognizers was added.

--- PlayingCard/PlayingCardView.swift
+++ PlayingCard/PlayingCardView.swift
@@ -6,19 +6,45 @@
 //  Copyright © 2020 Valentin Shapovalov. All rights reserved.
 //
 
+
 import UIKit
 
 @IBDesignable
-class PlayingCardView: UIView {
+class PlayingCardView: UIView
+{
     @IBInspectable
-    var rank: Int = 5 {didSet { setNeedsDisplay(); setNeedsLayout() }}
+    var rank: Int = 12 { didSet { setNeedsDisplay(); setNeedsLayout() } }
     @IBInspectable
     var suit: String = "❤️" { didSet { setNeedsDisplay(); setNeedsLayout() } }
     @IBInspectable
     var isFaceUp: Bool = true { didSet { setNeedsDisplay(); setNeedsLayout() } }
+
+    @IBInspectable
+    var faceCardScale: CGFloat = SizeRatio.faceCardImageSizeToBoundsSize { didSet { setNeedsDisplay() } }
     
-    lazy private var upperLeftCornerLabel = createCornerLabel()
-    lazy private var lowerRightCornerLabel = createCornerLabel()
+    @objc func adjustFaceCardScale(byHandlingGestureRecognizedBy recognizer: UIPinchGestureRecognizer) {
+        switch recognizer.state {
+        case .changed,.ended:
+            faceCardScale *= recognizer.scale
+            recognizer.scale = 1.0
+        default: break
+        }
+    }
+    
+    private func centeredAttributedString(_ string: String, fontSize: CGFloat) -> NSAttributedString {
+        var font = UIFont.preferredFont(forTextStyle: .body).withSize(fontSize)
+        font = UIFontMetrics(forTextStyle: .body).scaledFont(for: font)
+        let paragraphStyle = NSMutableParagraphStyle()
+        paragraphStyle.alignment = .center
+        return NSAttributedString(string: string, attributes: [.paragraphStyle:paragraphStyle,.font:font])
+    }
+    
+    private var cornerString: NSAttributedString {
+        return centeredAttributedString(rankString+"\n"+suit, fontSize: cornerFontSize)
+    }
+    
+    private lazy var upperLeftCornerLabel = createCornerLabel()
+    private lazy var lowerRightCornerLabel = createCornerLabel()
     
     private func createCornerLabel() -> UILabel {
         let label = UILabel()
@@ -27,23 +53,6 @@ class PlayingCardView: UIView {
         return label
     }
     
-    
-    
-    private var cornerString: NSAttributedString {
-        return centeredAttributedString(rankString + "\n" + suit, fontSize: cornerFontSize)
-    }
-    
-    private func centeredAttributedString(_ string: String,
-                                          fontSize: CGFloat) -> NSAttributedString {
-        var font = UIFont.preferredFont(forTextStyle: .body).withSize(fontSize)
-        font = UIFontMetrics(forTextStyle: .body).scaledFont(for: font)
-        let paragraphStyle = NSMutableParagraphStyle()
-        paragraphStyle.alignment = .center
-        return NSAttributedString(string: string,
-                                  attributes: [.paragraphStyle: paragraphStyle, .font: font])
-    }
-    
-    
     private func configureCornerLabel(_ label: UILabel) {
         label.attributedText = cornerString
         label.frame.size = CGSize.zero
@@ -51,6 +60,26 @@ class PlayingCardView: UIView {
         label.isHidden = !isFaceUp
     }
     
+    override func traitCollectionDidChange(_ previousTraitCollection: UITraitCollection?) {
+
+        setNeedsDisplay()
+        setNeedsLayout()
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        
+        configureCornerLabel(upperLeftCornerLabel)
+        upperLeftCornerLabel.frame.origin = bounds.origin.offsetBy(dx: cornerOffset, dy: cornerOffset)
+        
+        configureCornerLabel(lowerRightCornerLabel)
+        lowerRightCornerLabel.transform = CGAffineTransform.identity
+            .translatedBy(x: lowerRightCornerLabel.frame.size.width, y: lowerRightCornerLabel.frame.size.height)
+            .rotated(by: CGFloat.pi)
+        lowerRightCornerLabel.frame.origin = CGPoint(x: bounds.maxX, y: bounds.maxY)
+            .offsetBy(dx: -cornerOffset, dy: -cornerOffset)
+            .offsetBy(dx: -lowerRightCornerLabel.frame.size.width, dy: -lowerRightCornerLabel.frame.size.height)
+    }
     
     private func drawPips()
     {
@@ -91,18 +120,16 @@ class PlayingCardView: UIView {
             }
         }
     }
-    
-    
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
+
     override func draw(_ rect: CGRect) {
         let roundedRect = UIBezierPath(roundedRect: bounds, cornerRadius: cornerRadius)
         roundedRect.addClip()
         UIColor.white.setFill()
         roundedRect.fill()
+        
         if isFaceUp {
-            if let faceCardImage = UIImage(named: rankString + suit, in: Bundle(for: self.classForCoder), compatibleWith: traitCollection) {
-                faceCardImage.draw(in: bounds.zoom(by: SizeRatio.faceCardImageSizeToBoundsSize))
+            if let faceCardImage = UIImage(named: rankString+suit, in: Bundle(for: self.classForCoder), compatibleWith: traitCollection) {
+                faceCardImage.draw(in: bounds.zoom(by: faceCardScale))
             } else {
                 drawPips()
             }
@@ -112,28 +139,6 @@ class PlayingCardView: UIView {
             }
         }
     }
-    
-    override func layoutSubviews() {
-        super.layoutSubviews()
-        
-        configureCornerLabel(upperLeftCornerLabel)
-        upperLeftCornerLabel.frame.origin = bounds.origin.offsetBy(dx: cornerOffset, dy: cornerOffset)
-        
-        configureCornerLabel(lowerRightCornerLabel)
-        lowerRightCornerLabel.transform = CGAffineTransform.identity.rotated(by: CGFloat.pi).translatedBy(x: lowerRightCornerLabel.frame.size.width, y: lowerRightCornerLabel.frame.size.height)
-        lowerRightCornerLabel.frame.origin = CGPoint(x: bounds.maxX, y: bounds.maxY)
-            .offsetBy(dx: -cornerOffset, dy: -cornerOffset)
-            .offsetBy(dx: -lowerRightCornerLabel.frame.size.width, dy: -lowerRightCornerLabel.frame.size.height)
-    }
-    
-    override func traitCollectionDidChange(_ previousTraitCollection: UITraitCollection?) {
-        setNeedsDisplay()
-        setNeedsLayout()
-    }
-    
-    
-    
-    
 }
 
 extension PlayingCardView {
@@ -189,4 +194,5 @@ extension CGPoint {
         return CGPoint(x: x+dx, y: y+dy)
     }
 }
+
 
--- PlayingCard/ViewController.swift
+++ PlayingCard/ViewController.swift
@@ -9,8 +9,27 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     var deck = PlayingCardDeck()
+    
+    @IBOutlet weak var playingCardView: PlayingCardView! {
+        didSet {
+            let swipe = UISwipeGestureRecognizer(target: self, action: #selector(nextCard))
+            swipe.direction = [.left,.right]
+            playingCardView.addGestureRecognizer(swipe)
+            
+            let pinch = UIPinchGestureRecognizer(target: playingCardView, action: #selector(PlayingCardView.adjustFaceCardScale(byHandlingGestureRecognizedBy:)))
+            playingCardView.addGestureRecognizer(pinch)
+        }
+    }
+    
+    @objc func nextCard() {
+        if let card = deck.draw() {
+            playingCardView.rank = card.rank.order
+            playingCardView.suit = card.suit.rawValue
+        }
+    }
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,7 +39,14 @@ class ViewController: UIViewController {
             }
         }
     }
-
-
+    
+    @IBAction func flipCard(_ sender: UITapGestureRecognizer) {
+        switch sender.state {
+        case .ended:
+            playingCardView.isFaceUp = !playingCardView.isFaceUp
+        default: break
+        }
+    }
+    
 }
 
